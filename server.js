61a4e9a122ce97ee71ed63df7dbc2f9b5de70484
Bonnie <bonnieacuna@gmail.com>
2019-03-13 18:52:22 -0700

    db update

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
 const axios = require("axios");
 const cheerio = require("cheerio");
+
+const db = require("./models");
 
 const PORT = process.env.PORT || 3000;
 
@@ -24,7 +25,6 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLin
 
 mongoose.connect(MONGODB_URI);
 
-const db = require("./models");
 app.get("/", function (req, res) {
     db.Article.find({})
         .then(function (dbArticle) {
